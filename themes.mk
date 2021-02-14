ee2d18341c6cb3f69355582057affdd5f14fdd39
bezke <bezke1@gmail.com>
2021-02-14 12:39:06 +0100

    Add brightness, qsshape, panelbg style [3/3]

---
+++ themes.mk
@@ -0,0 +1,98 @@
+# Themes
+PRODUCT_PACKAGES += \
+    PitchBlackSystemOverlay \
+    PitchBlackSystemUIOverlay \
+    SolarizedDarkSystemOverlay \
+    SolarizedDarkSystemUIOverlay \
+    MaterialOceanSystemOverlay \
+    MaterialOceanSystemUIOverlay \
+    NadClearSystemOverlay \
+    NadClearSystemUIOverlay
+
+# Icon shape
+PRODUCT_PACKAGES += \
+    IconShapePebbleOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay \
+    IconShapeHeartOverlay
+
+# Navbar
+PRODUCT_PACKAGES += \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarTecnoCamonOverlay \
+    NavbarAndroidOverlay \
+    NavbarMotoOverlay \
+    NavbarNexusOverlay \
+    NavbarOldOverlay \
+    NavbarSammyOverlay
+
+PRODUCT_PACKAGES += \
+    GesturalNavigationOverlayLong \
+    GesturalNavigationOverlayMedium \
+    GesturalNavigationOverlayHidden
+    
+PRODUCT_PACKAGES += \
+    GesturalNavigationRadiusLow \
+    GesturalNavigationRadiusVeryLow \
+    GesturalNavigationRadiusHidden
+    
+PRODUCT_PACKAGES += \
+    GesturalNavigationMarginLow \
+    GesturalNavigationMarginHeight \
+    GesturalNavigationMarginNull
+
+# QS header styles
+PRODUCT_PACKAGES += \
+    QSHeaderGrey \
+    QSHeaderLightGrey \
+    QSHeaderAccent \
+    QSHeaderTransparent
+
+# GVM
+PRODUCT_PACKAGES += \
+    GVM-SBH-L \
+    GVM-SBH-M \
+    GVM-SBH-XL \
+    GVM-URM-M \
+    GVM-URM-L \
+    GVM-URM-R
+
+# Panel Background
+PRODUCT_PACKAGES += \
+    PanelBgBatik \
+    PanelBgKece \
+    PanelBgOutline
+
+# Qs shape
+PRODUCT_PACKAGES += \
+    QsShapeA \
+    QsShapeB \
+    QsShapeC \
+    QsShapeD \
+    QsShapeE \
+    QsShapeF \
+    QsShapeG \
+    QsShapeH \
+    QsShapeI \
+    QsShapeJ \
+    QsShapeK \
+    QsShapeL \
+    QsShapeM \
+    QsShapeN \
+    QsShapeO \
+    QsShapeP \
+    QsShapeQ \
+    QsShapeR \
+    QsShapeS 
+
+# Brightness sliders Styles
+PRODUCT_PACKAGES += \
+    BrightnessTypeA \
+    BrightnessTypeB \
+    BrightnessTypeC \
+    BrightnessTypeD  
